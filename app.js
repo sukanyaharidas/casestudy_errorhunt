0853f8c167fd62764d817e4915c4e1ebdacdbc0b
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-05-06 10:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.get('/',function(req,res){
  
 
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server Ready on 3000");// Part #1 Point 5-->server ready on 5000
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Server Ready on 5000");// Part #1 Point 5-->server ready on 5000
 });
